7d3dfe63af3d2326cdf6a41e57daf4f49083b72b
ricardo <ricardo.baumann@krieger.de>
2023-02-14 13:04:50 +0100

    keycloak done

---
+++ get_token.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+curl \
+  -d "client_id=admin-cli" \
+  -d "username=admin" \
+  -d "password=passwd" \
+  -d "grant_type=password" \
+  "http://localhost:28080/auth/realms/master/protocol/openid-connect/token"
