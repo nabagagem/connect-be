538c939c9436dc7274ec6cef5f96a697d0b163df
ricardo <ricardo.baumann@krieger.de>
2023-02-15 10:57:47 +0100

    deployment script

--- deploy-docker.sh
+++ deploy-docker.sh
@@ -3,4 +3,3 @@ docker build -t connect-be:latest .
 aws ecr-public get-login-password --region us-east-1 | docker login --username AWS --password-stdin public.ecr.aws
 docker tag connect-be:latest public.ecr.aws/l7g1e1z0/connect-be:latest
 docker push public.ecr.aws/l7g1e1z0/connect-be:latest
-q
