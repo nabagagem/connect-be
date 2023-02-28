917144625bfb2987e0044f4b566b0b715738ef00
ricardo <ricardo.baumann@krieger.de>
2023-02-28 10:59:32 +0100

    keycloak themes

--- src/main/java/com/nabagagem/connectbe/resources/GigResource.java
+++ src/main/java/com/nabagagem/connectbe/resources/GigResource.java
@@ -7,7 +7,7 @@ import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import java.util.Set;
 import java.util.UUID;
 
-@SuppressWarnings("SpringDataRepositoryMethodParametersInspection")
+@SuppressWarnings({"SpringDataRepositoryMethodParametersInspection", "unused"})
 @RepositoryRestResource
 public interface GigResource extends CrudRepository<Gig, UUID> {
     Set<Gig> findByTagsIn(Set<String> tags);
