ea139737a1d08589f107da24d5d708afbfccdd63
ricardo <ricardo.baumann@krieger.de>
2023-04-16 18:39:47 +0200

    profile setup

--- src/test/java/com/nabagagem/connectbe/SampleJpaTest.java
+++ src/test/java/com/nabagagem/connectbe/SampleJpaTest.java
@@ -1,6 +1,7 @@
 package com.nabagagem.connectbe;
 
 import lombok.SneakyThrows;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -9,6 +10,7 @@ import org.springframework.test.web.servlet.MockMvc;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+@Disabled
 @AutoConfigureMockMvc
 public class SampleJpaTest extends BaseJpaTest {
 
