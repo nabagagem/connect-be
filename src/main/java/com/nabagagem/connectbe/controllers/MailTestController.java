0a247299836868d1fd9ab9daf5c582e9b9daa039
ricardo <ricardo.baumann@krieger.de>
2023-04-21 09:36:03 +0200

    mail test controller

---
+++ src/main/java/com/nabagagem/connectbe/controllers/MailTestController.java
@@ -0,0 +1,23 @@
+package com.nabagagem.connectbe.controllers;
+
+import lombok.AllArgsConstructor;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@AllArgsConstructor
+public class MailTestController {
+    private final JavaMailSender javaMailSender;
+
+    @GetMapping("/test/mail")
+    public void send() {
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setFrom("gemini.richard@gmail.com");
+        message.setTo("gemini.richard@gmail.com");
+        message.setSubject("test");
+        message.setText("test");
+        javaMailSender.send(message);
+    }
+}
