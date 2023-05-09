caf206392be9fc3470c9cd8f9e69ec7e3cde0584
ricardo <ricardo.baumann@krieger.de>
2023-05-09 17:23:20 +0200

    web socket scaffold

--- src/main/java/com/nabagagem/connectbe/controllers/UiOptions.java
+++ src/main/java/com/nabagagem/connectbe/controllers/UiOptions.java
@@ -1,21 +1,16 @@
 package com.nabagagem.connectbe.controllers;
 
 import com.nabagagem.connectbe.controllers.UiSupportController.CategoryTree;
-import com.nabagagem.connectbe.domain.AvailabilityType;
-import com.nabagagem.connectbe.domain.JobFrequency;
-import com.nabagagem.connectbe.domain.JobMode;
-import com.nabagagem.connectbe.domain.JobRequiredAvailability;
-import com.nabagagem.connectbe.domain.JobSize;
-import com.nabagagem.connectbe.domain.JobStatus;
-import com.nabagagem.connectbe.domain.WorkingMode;
+import com.nabagagem.connectbe.domain.*;
 import com.nabagagem.connectbe.entities.ProfileCategory;
 
+import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
 public record UiOptions(
         Map<AvailabilityType, String> availabilityTypes,
-        java.util.List<CategoryTree> jobCategories,
+        List<CategoryTree> jobCategories,
         Map<JobSize, String> jobSizes,
         Map<JobFrequency, String> jobFrequencies,
         Map<JobMode, String> jobModes,
