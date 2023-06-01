58264ec24b418fbd75cecbf00c4aa50aaa7fa046
ricardo <ricardo.baumann@krieger.de>
2023-06-01 13:25:33 +0200

    etag support

---
+++ src/main/java/com/nabagagem/connectbe/config/EtagConfig.java
@@ -0,0 +1,13 @@
+package com.nabagagem.connectbe.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.filter.ShallowEtagHeaderFilter;
+
+@Configuration
+public class EtagConfig {
+    @Bean
+    public ShallowEtagHeaderFilter shallowEtagHeaderFilter() {
+        return new ShallowEtagHeaderFilter();
+    }
+}
--- src/main/java/com/nabagagem/connectbe/repos/TestController.java
+++ src/main/java/com/nabagagem/connectbe/repos/TestController.java
@@ -9,8 +9,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.UUID;
-
 @Slf4j
 @RestController
 @Validated
@@ -18,12 +16,12 @@ public class TestController {
 
     @GetMapping("/test")
     Test test() {
-        return new Test("bar", UUID.randomUUID().toString());
+        return new Test("bar", "id");
     }
 
     @PostMapping("/test")
     Test test(@RequestBody @Valid TestInput testInput) {
-        return new Test(testInput.foo(), UUID.randomUUID().toString());
+        return new Test(testInput.foo(), "id");
     }
 
     private record Test(String foo, String id) {
