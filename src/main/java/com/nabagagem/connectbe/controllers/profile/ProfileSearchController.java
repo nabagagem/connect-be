aca4f99e08dae1c315baff9851cbcd6c59dad4c3
ricardo <ricardo.baumann@krieger.de>
2023-06-02 16:19:09 +0200

    fix enums

--- src/main/java/com/nabagagem/connectbe/controllers/profile/ProfileSearchController.java
+++ src/main/java/com/nabagagem/connectbe/controllers/profile/ProfileSearchController.java
@@ -7,7 +7,6 @@ import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -20,7 +19,7 @@ public class ProfileSearchController {
 
     @GetMapping
     public Page<ProfileSearchItemPayload> list(@Valid ProfileSearchParams profileSearchParams,
-                                               @PageableDefault(size = 10) Pageable pageable) {
+                                               Pageable pageable) {
         return profileSearchService.searchFor(profileSearchParams, pageable);
     }
 
