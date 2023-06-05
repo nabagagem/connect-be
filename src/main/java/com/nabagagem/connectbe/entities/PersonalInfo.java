e2721355d6c30f1c190671d637fb54a60808f383
Ricardo Luis Baumann <gemini.richard@gmail.com>
2023-06-05 10:40:56 +0200

    RAM-29: profile email (#13)
    Co-authored-by: ricardo <ricardo.baumann@krieger.de>

--- src/main/java/com/nabagagem/connectbe/entities/PersonalInfo.java
+++ src/main/java/com/nabagagem/connectbe/entities/PersonalInfo.java
@@ -8,6 +8,7 @@ import jakarta.persistence.Embedded;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
 import jakarta.validation.Valid;
+import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
@@ -45,4 +46,7 @@ public class PersonalInfo {
     @Valid
     @NotNull
     private MoneyAmount amountPerHour;
+    @Email
+    @Column(unique = true)
+    private String email;
 }
