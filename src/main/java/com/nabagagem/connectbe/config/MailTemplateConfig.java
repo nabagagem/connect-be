0d1034e0ac8f2e511d1628dc71ac0cd2e2af311a
ricardo <ricardo.baumann@krieger.de>
2023-06-05 12:06:39 +0200

    RAM-19: notification email wip

---
+++ src/main/java/com/nabagagem/connectbe/config/MailTemplateConfig.java
@@ -0,0 +1,37 @@
+package com.nabagagem.connectbe.config;
+
+import lombok.AllArgsConstructor;
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.spring6.SpringTemplateEngine;
+import org.thymeleaf.templatemode.TemplateMode;
+import org.thymeleaf.templateresolver.ClassLoaderTemplateResolver;
+import org.thymeleaf.templateresolver.ITemplateResolver;
+
+import java.util.Collections;
+
+@Configuration
+@AllArgsConstructor
+public class MailTemplateConfig {
+    private final MessageSource messageSource;
+
+    @Bean
+    public TemplateEngine emailTemplateEngine() {
+        final SpringTemplateEngine templateEngine = new SpringTemplateEngine();
+        templateEngine.addTemplateResolver(htmlTemplateResolver());
+        templateEngine.setTemplateEngineMessageSource(messageSource);
+        return templateEngine;
+    }
+
+    private ITemplateResolver htmlTemplateResolver() {
+        final ClassLoaderTemplateResolver templateResolver = new ClassLoaderTemplateResolver();
+        templateResolver.setResolvablePatterns(Collections.singleton("*"));
+        templateResolver.setSuffix(".html");
+        templateResolver.setTemplateMode(TemplateMode.HTML);
+        templateResolver.setCharacterEncoding("UTF-8");
+        templateResolver.setCacheable(false);
+        return templateResolver;
+    }
+}
