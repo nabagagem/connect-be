6940306e3c9b8f6447b2b9c309f8084c70d26999
ricardo <ricardo.baumann@krieger.de>
2023-07-10 09:34:03 +0200

    fix message mapper

--- src/main/java/com/nabagagem/connectbe/services/mappers/MessageMapper.java
+++ src/main/java/com/nabagagem/connectbe/services/mappers/MessageMapper.java
@@ -3,11 +3,16 @@ package com.nabagagem.connectbe.services.mappers;
 import com.nabagagem.connectbe.domain.ThreadMessage;
 import com.nabagagem.connectbe.entities.Message;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.MappingConstants;
 import org.mapstruct.ReportingPolicy;
 
 @Mapper(unmappedTargetPolicy = ReportingPolicy.IGNORE, componentModel = MappingConstants.ComponentModel.SPRING,
         uses = {ReactionMapper.class})
 public interface MessageMapper {
+
+    @Mapping(target = "sentAt", source = "audit.createdAt")
+    @Mapping(target = "sentBy", source = "audit.createdBy")
+    @Mapping(target = "message", source = "text")
     ThreadMessage toDto(Message message);
 }
