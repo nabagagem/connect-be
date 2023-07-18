fc5acc03d5c89dfbab026e9beeae41d786ea7400
ricardo <ricardo.baumann@krieger.de>
2023-07-18 11:04:20 +0200

    WS token support

--- src/main/java/com/nabagagem/connectbe/config/WebSocketConfig.java
+++ src/main/java/com/nabagagem/connectbe/config/WebSocketConfig.java
@@ -1,16 +1,35 @@
 package com.nabagagem.connectbe.config;
 
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.MessageChannel;
+import org.springframework.messaging.MessageHeaders;
+import org.springframework.messaging.simp.config.ChannelRegistration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
+import org.springframework.messaging.support.ChannelInterceptor;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+
+@Slf4j
 @Configuration
 @EnableWebSocketMessageBroker
+@AllArgsConstructor
 @ConditionalOnProperty("ramifica.web-socket.enabled")
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+    private final JwtDecoder jwtDecoder;
 
     @Override
     public void configureMessageBroker(MessageBrokerRegistry config) {
@@ -21,6 +40,47 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/ws").setAllowedOriginPatterns("*");
-        registry.addEndpoint("/ws").setAllowedOriginPatterns("*").withSockJS();
+        registry.addEndpoint("/ws").setAllowedOriginPatterns("*")
+                .withSockJS();
+    }
+
+    @Override
+    public void configureClientInboundChannel(ChannelRegistration registration) {
+        registration.interceptors(new ChannelInterceptor() {
+            private final Map<String, String> sessionTokens = new HashMap<>();
+
+            @SuppressWarnings({"unchecked", "DataFlowIssue"})
+            @Override
+            public Message<?> preSend(Message<?> message, MessageChannel channel) {
+                MessageHeaders headers = message.getHeaders();
+                String simpMessageType = headers.getOrDefault("simpMessageType", "").toString();
+                MultiValueMap<String, String> multiValueMap = headers.get(StompHeaderAccessor.NATIVE_HEADERS, MultiValueMap.class);
+                String simpSessionId = headers.get("simpSessionId").toString();
+                if (simpMessageType.equals("CONNECT")) {
+                    sessionTokens.put(simpSessionId, multiValueMap.getFirst("Token"));
+                }
+                if (simpMessageType.equals("SUBSCRIBE")) {
+                    String destination = multiValueMap.getFirst("destination");
+                    String[] destinationParts = destination
+                            .split("/");
+                    String topicUserId = destinationParts[destinationParts.length - 1];
+                    log.info("Topic user id: {}", topicUserId);
+                    Optional.ofNullable(sessionTokens.get(simpSessionId))
+                            .ifPresentOrElse(token -> {
+                                log.info("Token for user session: {}", token);
+                                Jwt jwt = jwtDecoder.decode(token);
+                                String sub = jwt.getClaim("sub").toString();
+                                if (!sub.equals(topicUserId)) {
+                                    log.warn("Session token sub does not match the topic user id: {} - {}", sub, topicUserId);
+                                    throw new AccessDeniedException("Unauthorized");
+                                }
+                            }, () -> {
+                                log.warn("No token set on session: {}", simpSessionId);
+                                throw new AccessDeniedException("Unauthorized");
+                            });
+                }
+                return message;
+            }
+        });
     }
 }
--- ws-client/app.js
+++ ws-client/app.js
@@ -15,7 +15,7 @@ function connect() {
     //var socket = new SockJS('http://localhost:8080/ws');
     var socket = new SockJS('http://localhost:8080/ws');
     stompClient = Stomp.over(socket);
-    stompClient.connect({}, function (frame) {
+    stompClient.connect({'Token': 'eyJhbGciOiJSUzI1NiIsInR5cCIgOiAiSldUIiwia2lkIiA6ICJ1eVg5U0o4eVNJTjlzZVBFSnlKY0ttWlZIMlpFYXg1X1EtQXgzYUpvUGFnIn0.eyJleHAiOjE2ODk2OTg4ODUsImlhdCI6MTY4OTY2NDAzOCwiYXV0aF90aW1lIjoxNjg5NjYyODg2LCJqdGkiOiJkMDIzOGQ2My03MTAxLTQzMjktODk3My1lZjQzY2ZmOTVlOTUiLCJpc3MiOiJodHRwOi8vbG9jYWxob3N0OjI4MDgwL2F1dGgvcmVhbG1zL21hc3RlciIsInN1YiI6ImM2Mjk3NzNlLTVmODUtNDczMS1iZGZjLWEyNmYzOWYxYWRkOCIsInR5cCI6IkJlYXJlciIsImF6cCI6ImFkbWluLWNsaSIsInNlc3Npb25fc3RhdGUiOiIwOWRkYzE5Ni0xYjM2LTQ4NzctOWVjYy0xNDgxM2ZmYjE5MzkiLCJhY3IiOiIwIiwiYWxsb3dlZC1vcmlnaW5zIjpbIioiXSwic2NvcGUiOiJwcm9maWxlIGVtYWlsIiwic2lkIjoiMDlkZGMxOTYtMWIzNi00ODc3LTllY2MtMTQ4MTNmZmIxOTM5IiwiZW1haWxfdmVyaWZpZWQiOmZhbHNlLCJuYW1lIjoiUmljYXJkbyBCYXVtYW5uIiwicHJlZmVycmVkX3VzZXJuYW1lIjoiYWRtaW4iLCJnaXZlbl9uYW1lIjoiUmljYXJkbyIsImZhbWlseV9uYW1lIjoiQmF1bWFubiJ9.cMg5zWlS9Mg3Ar_8XTJm45sZec7tb5NWuvkTPmhBX57kHQ2VXimbsxGiedWc8wI5nMlVyHiPXnWHI54Hem3mE8ajPOA5SCtFHgQfx6Kc8eLN_KLWHgO8LxdbuMgliVnAoM0PrkGgHG7FhSvd6qI1taiFlfrOghL-UZnK_chWbhn6rGFbD_BbMKW4gzPNC8PmcAi5cJ4OI2GIax3W7OkgZ-8YN0bxr9yzqtQCh13sNJbsIZn4CPEqOzmkNJbRQQpWEaoiN6RxZy-jMYCz4dwNt6-uWM1sAsCuY7wxLQHKjwvc8IYrGbOevIElIBiTayAzOk71fTMfGvOLi8f73QS6NA'}, function (frame) {
         setConnected(true);
         console.log('Connected: ' + frame);
         stompClient.subscribe('/topics/user/c629773e-5f85-4731-bdfc-a26f39f1add8', function (greeting) {
