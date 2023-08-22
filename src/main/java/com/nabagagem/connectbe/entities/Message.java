a3d18fb57db7242f651c656f772f502929f498cc
ricardo <ricardo.baumann@krieger.de>
2023-08-22 20:58:52 +0200

    audio upload endpoint

--- src/main/java/com/nabagagem/connectbe/entities/Message.java
+++ src/main/java/com/nabagagem/connectbe/entities/Message.java
@@ -19,9 +19,7 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
-import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.util.Set;
@@ -31,8 +29,6 @@ import java.util.UUID;
 @Entity
 @AllArgsConstructor
 @NoArgsConstructor
-@Getter
-@Setter
 @Builder
 @EqualsAndHashCode(of = "id")
 @Table(name = "message", indexes = {
