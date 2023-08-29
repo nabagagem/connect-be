ad3115e70278ebf866c10f8c4513a5622a0dfb16
ricardo <ricardo.baumann@krieger.de>
2023-08-29 10:34:02 +0200

    log cleanup

--- src/main/java/com/nabagagem/connectbe/config/LastActivityFilter.java
+++ src/main/java/com/nabagagem/connectbe/config/LastActivityFilter.java
@@ -37,7 +37,6 @@ public class LastActivityFilter implements Filter, LoggedUserIdTrait {
             String requestURI = httpServletRequest.getRequestURI();
             if (requestURI.startsWith("/api")) {
                 String method = httpServletRequest.getMethod();
-                log.info("API Request: {} {}", method, requestURI);
                 loggedUser()
                         .ifPresent(loggedUserId -> threadPoolTaskExecutor
                                 .submit(() -> lastActivityService.register(loggedUserId)));
