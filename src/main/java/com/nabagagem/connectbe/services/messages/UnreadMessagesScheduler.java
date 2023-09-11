1da3c717aec5a5b53136c20ab58bf9ca941d8640
ricardo <ricardo.baumann@krieger.de>
2023-09-11 15:54:44 +0200

    RAM-121: unread mail notifications

--- src/main/java/com/nabagagem/connectbe/services/messages/UnreadMessagesScheduler.java
+++ src/main/java/com/nabagagem/connectbe/services/messages/UnreadMessagesScheduler.java
@@ -4,6 +4,7 @@ import com.nabagagem.connectbe.repos.MessageRepo;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 import org.springframework.stereotype.Service;
 
 import java.time.ZonedDateTime;
@@ -15,11 +16,13 @@ import java.util.concurrent.TimeUnit;
 public class UnreadMessagesScheduler {
     private final MessageRepo messageRepo;
     private final UnreadMessageService unreadMessageService;
+    private final ThreadPoolTaskExecutor threadPoolTaskExecutor;
 
     @Scheduled(fixedDelay = 10, timeUnit = TimeUnit.MINUTES)
     public void run() {
         messageRepo.findProfilesWithUnreadMessages(ZonedDateTime.now().minusMinutes(10))
                 .stream().peek(profile -> log.info("Profile have unread mails: {}", profile.getId()))
-                .forEach(unreadMessageService::sendUnreadEmail);
+                .forEach(profileMailPersonalInfo -> threadPoolTaskExecutor
+                        .submit(() -> unreadMessageService.sendUnreadEmail(profileMailPersonalInfo)));
     }
 }
