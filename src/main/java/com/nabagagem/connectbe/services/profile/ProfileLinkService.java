b6d44fe55a9fe20b6bfb6504495beebeba52b8aa
ricardo <ricardo.baumann@krieger.de>
2023-11-01 22:17:57 +0100

    fix profile links

--- src/main/java/com/nabagagem/connectbe/services/profile/ProfileLinkService.java
+++ src/main/java/com/nabagagem/connectbe/services/profile/ProfileLinkService.java
@@ -2,6 +2,7 @@ package com.nabagagem.connectbe.services.profile;
 
 import com.nabagagem.connectbe.domain.profile.ProfileLinksCommand;
 import com.nabagagem.connectbe.entities.ConnectProfile;
+import com.nabagagem.connectbe.entities.LinkType;
 import com.nabagagem.connectbe.entities.ProfileLink;
 import com.nabagagem.connectbe.repos.ProfileLinkRepository;
 import lombok.AllArgsConstructor;
@@ -26,12 +27,32 @@ public class ProfileLinkService {
                         .entrySet().stream()
                         .filter(entry -> StringUtils.isNotBlank(entry.getValue()))
                         .map(entry -> ProfileLink.builder()
-                                .linkURL(entry.getValue())
+                                .linkURL(toURL(entry.getKey(), entry.getValue()))
                                 .linkType(entry.getKey())
                                 .profile(profile)
                                 .build())
                         .collect(Collectors.toSet())
         );
         profileService.save(profile);
+    }
+
+    private String toURL(LinkType linkType, String url) {
+        if (url.startsWith("https")) {
+            return url;
+        }
+        String[] parts = url.split("/");
+        final String lastPart = parts[parts.length - 1];
+        return switch (linkType) {
+            case X -> String.format("https://twitter.com/%s", lastPart);
+            case LINKEDIN -> String.format("https://linkedin.com/%s", getLinkedInUser(parts));
+            case FACEBOOK -> String.format("https://facebook.com/%s", lastPart);
+            default -> url;
+        };
+    }
+
+    private String getLinkedInUser(String[] parts) {
+        int length = parts.length;
+        String lastPart = parts[length - 1];
+        return lastPart.contains("locale=") && length >= 2 ? parts[length - 2] : lastPart;
     }
 }
